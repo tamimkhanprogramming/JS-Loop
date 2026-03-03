31d22ecaa276063d3e69c85a2aa7f5cf0ab2055b
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-04 00:58:29 +0600

    Updated

---
+++ break.js
@@ -0,0 +1,41 @@
+// for(let i = 0; i<15; i++){
+
+//     console.log(i);
+//     if(i>=10){
+        
+//         break;
+        
+//     }
+// }
+
+// for(let i = 0; i<20; i++){
+//     console.log(i);
+//     if(i>=15){
+//         break;
+//     }
+// }
+
+// console.log("BreakPoint Here!");
+
+// let n = 54;
+
+// while(n>25){
+        
+//     if(n<50){
+      
+       
+//        break;
+//     }
+//     console.log(n);
+//     n--;
+// }
+
+let n = 54;
+
+while(n>25){
+    console.log(n);
+    if(n<50){
+        break;
+    }
+    n--;
+}
---
+++ continue.js
@@ -0,0 +1,18 @@
+// for(let i = 0;i<10;i++){
+
+//     if(i%2===1){
+//         continue;
+//     }
+
+//     console.log(i);
+// }
+
+let n = 0;
+
+while(n<50){
+    n++;
+    if(n%5!==0){
+       continue;
+    }
+    console.log(n);
+}
