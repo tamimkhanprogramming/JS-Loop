f5e8dc9551a3015acb580f56d2b44ee79546ae20
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-03 06:39:27 +0600

    Main

---
+++ For.js
@@ -0,0 +1,30 @@
+for(let num = 0; num < 10; num++){
+
+    console.log(num);
+}
+
+for(let i = 0; i<=20; i++){
+
+    console.log(i);
+}
+
+// Even Number
+for(let i = 0; i<=20; i=i+2){
+
+    console.log(i);
+}
+
+// ODD Number
+for(let i = 1; i<=20; i=i+2){
+
+    console.log(i);
+}
+// Using While Loop
+let i = 1;
+
+while(i<10){
+
+    console.log(i);
+    i= i+2;
+}
+
