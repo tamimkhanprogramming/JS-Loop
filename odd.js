ccabaea30b7580bd3846810ad0372a774d714f6e
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-03 15:17:22 +0600

    Updated

---
+++ odd.js
@@ -0,0 +1,25 @@
+// for(let i = 0; i<20; i++){
+// if(i % 2 === 1){
+
+//     console.log("ODD Number",i);
+
+// }
+    
+// }
+
+// for(let i = 1; i<20; i++){
+
+//     if(i%2!==0){
+//           console.log("ODD Number",i);
+//     }
+// }
+
+// Give Me The List Numbers Between 1 to 30 Divisble by 5 or 3
+
+for(let i=0;i<=30;i++){
+
+    if(i % 5 === 0 || i % 3 === 0){
+
+        console.log(i);
+    }
+}
