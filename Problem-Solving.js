2dc2b4e993fa0a33e16358bbc12edd3b1fbd4468
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-03 07:00:46 +0600

    Updated

---
+++ Problem-Solving.js
@@ -0,0 +1,20 @@
+let sum = 0;
+for(let i = 11; i<=20; i++){
+
+    sum = sum + i;
+    console.log(i);
+}
+console.log("Sum of Total Number",sum);
+
+// Incremental Loop
+for(let i = 0; i<=10;i++){
+
+    console.log(i);
+}
+
+// Decremental Loop
+
+for(let i = 0; i<10; i--){
+
+    console.log(i);
+}
